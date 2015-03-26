174bf2d4eae3b6580ed8df5fae9a6ba115fe4b0e
Katelyn Gigante <clockwork.singularity@gmail.com>
2015-03-26 13:56:25 +1100

    Xwt project?

---
+++ CharacterStudio-XP/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xwt;
+
+namespace CharacterStudio_XP
+{
+    class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.Size = new Size(this.ScreenBounds.Width, this.ScreenBounds.Height);
+            this.Content = this.TopPanel = new HPaned();
+            this.TopPanel.HeightRequest = this.ScreenBounds.Height;
+            this.TopPanel.Panel1.Content = this.Header = new Xwt.Notebook();
+        }
+
+        public HPaned TopPanel { get; set; }
+
+        public Notebook Header { get; set; }
+    }
+}
---
+++ CharacterStudio-XP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xwt;
+
+namespace CharacterStudio_XP
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.Initialize(ToolkitType.Wpf);
+            var main = new MainWindow();
+            main.Show();
+            Application.Run();
+        }
+    }
+}
