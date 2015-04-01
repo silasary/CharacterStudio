05d3e228ed33c129068d9289c5ad894af218ebd3
Katelyn Gigante <clockwork.singularity@gmail.com>
2015-04-01 14:14:49 +1100

    More tweaking of the XWT build.

--- CharacterStudio-XP/Program.cs
+++ CharacterStudio-XP/Program.cs
@@ -12,10 +12,33 @@ namespace CharacterStudio_XP
         [STAThread]
         static void Main(string[] args)
         {
-            Application.Initialize(ToolkitType.Wpf);
+            var platform = GetPlatform ();
+            if (platform.Platform == PlatformID.Win32NT)
+                Application.Initialize (ToolkitType.Wpf);
+            else // We'll get to Linux/GTK soon enough.
+                Application.Initialize (ToolkitType.Cocoa);
             var main = new MainWindow();
             main.Show();
             Application.Run();
+        }
+
+        public static OperatingSystem GetPlatform()
+        {
+            switch (Environment.OSVersion.Platform) {
+
+                case PlatformID.Unix:
+                    if (System.IO.Directory.Exists ("/Library"))
+                        return new OperatingSystem (PlatformID.MacOSX, new Version (10,0)); // We should try to work that out.
+                    else
+                        return Environment.OSVersion;
+                    break;
+
+                case PlatformID.MacOSX: // Wow, they actually got it!
+                case PlatformID.Win32NT:
+                default:
+                    return Environment.OSVersion;
+
+            }
         }
     }
 }
