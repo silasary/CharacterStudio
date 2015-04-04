97cb8cc761e184b836cd038ddd8f18d98ba836ed
Katelyn Gigante <clockwork.singularity@gmail.com>
2015-04-04 16:54:33 +1100

    Portraits

--- CharacterStudio/Controls/Common/SimpleCharacterVis.Designer.cs
+++ CharacterStudio/Controls/Common/SimpleCharacterVis.Designer.cs
@@ -47,6 +47,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(16, 11);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
--- CharacterStudio/Controls/Common/SimpleCharacterVis.cs
+++ CharacterStudio/Controls/Common/SimpleCharacterVis.cs
@@ -1,6 +1,9 @@
 ﻿using CharacterStudio.Controls.Panes;
+using System;
+using System.IO;
 using System.Windows.Forms;
 using System.Xml;
+using System.Linq;
 
 namespace CharacterStudio.Controls.Common
 {
@@ -78,18 +81,40 @@ namespace CharacterStudio.Controls.Common
                                 this.Class = reader.GetAttribute("name").Trim();
                             if (reader.GetAttribute("type") == "Race")
                                 this.Race = reader.GetAttribute("name").Trim();
+                            if (reader.GetAttribute("type") == "Gender")
+                                this.Gender = reader.GetAttribute("name").Trim();
 
                             break;
                     }
             }
             // TODO: Race/Class
             reader.Close();
+            if (string.IsNullOrWhiteSpace(Image))
+                Image = DefaultPortrait(Class,Race, Gender);
             if (!string.IsNullOrWhiteSpace(Image))
             {
                 this.pictureBox1.ImageLocation = Image;
                 //this.pictureBox1.ErrorImage = // Fallack to default for Race/Class/Etc?
             }
+
             this.label1.Text = string.Format("{0}\n Level {1} {2} {3}", Name, Level, Race, Class);
+        }
+
+        // TODO: Move this method somewhere better.
+        public static string DefaultPortrait(string Class, string Race, string Gender)
+        {
+            var ClassPort = String.Format("ClassPort{0}", Class).ToLower();
+            var RaceGendered = string.Format("{0}_{1}",(Gender ?? "").FirstOrDefault(), Race).ToLower();
+            var RacePort = string.Format("RacePort{0}", Race).ToLower();
+            var Generic = "Generic".ToLower();
+            var folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Character Studio", "Character Portraits");
+            Directory.CreateDirectory(folder);
+            var files = Directory.EnumerateFiles(folder, "*.*", SearchOption.AllDirectories).Select(n => n.ToLower());
+            var port = files.Where(n => n.Contains(ClassPort)).FirstOrDefault()
+                ?? files.Where(n => n.Contains(RaceGendered)).FirstOrDefault()
+                ?? files.Where(n => n.Contains(RacePort)).FirstOrDefault()
+                ?? files.Where(n => n.Contains(Generic)).FirstOrDefault();
+            return port;
         }
 
         private void SimpleCharacterVis_DoubleClick(object sender, System.EventArgs e)
@@ -101,5 +126,7 @@ namespace CharacterStudio.Controls.Common
         public string Class { get; set; }
 
         public string Race { get; set; }
+
+        public string Gender { get; set; }
     }
 }
