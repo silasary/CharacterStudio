692d7b78a70f4add3c5b7f6d64aa3e55a4c163b1
Katelyn Gigante <clockwork.singularity@gmail.com>
2015-05-25 14:24:49 +1000

    TestGrammar.cs was missing

---
+++ UnitTests/TestGrammar.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using ParagonLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    class TestGrammar
+    {
+        [Test]
+        public void TestAttackLine()
+        {
+            GrammarParser.ParsePowerLines(new ParagonLib.RuleBases.Power.PowerLine("Attack", "Strength vs AC"));
+        }
+    }
+}
