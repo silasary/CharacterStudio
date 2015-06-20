ca3017f2e686f3909284492d336048ea823c96d5
Katelyn Gigante <clockwork.singularity@gmail.com>
2015-06-20 23:31:16 +1000

    Hopefully this makes iPlay4e happy: Implemented the ugly and messy statlink attributes.

--- ParagonLib/Workspace.Serialization.cs
+++ ParagonLib/Workspace.Serialization.cs
@@ -24,12 +24,37 @@ namespace ParagonLib
                 foreach (var bit in bits)
                 {
                     writer.WriteStartElement("statadd");
-                    foreach (var field in typeof(bit).GetFields())
+                    if (SaveFileVersion > Serializer.SFVersion.v007b)
                     {
-                        if (!string.IsNullOrEmpty( field.GetValue(bit) as string))
-                            writer.WriteAttributeString(field.Name, (string)field.GetValue(bit));
-                        if (field.GetValue(bit) is int && (int)field.GetValue(bit) != 0) // Level and charelem
-                            writer.WriteAttributeString(field.Name, field.GetValue(bit).ToString());
+                        foreach (var field in typeof(bit).GetFields())
+                        {
+                            if (!string.IsNullOrEmpty(field.GetValue(bit) as string))
+                                writer.WriteAttributeString(field.Name, (string)field.GetValue(bit));
+                            if (field.GetValue(bit) is int && (int)field.GetValue(bit) != 0) // Level and charelem
+                                writer.WriteAttributeString(field.Name, field.GetValue(bit).ToString());
+                        }
+                    }
+                    else
+                    {
+                        //<statadd Level="1" value="1" statlink="Intelligence" abilmod="true" charelem="e93ad38" />
+                        if (bit.Level > 0)
+                            writer.WriteAttributeString("Level", bit.Level.ToString());
+                        int val;
+                        if (int.TryParse(bit.value, out val))
+                            writer.WriteAttributeString("value", bit.value.ToString());
+                        else
+                        {
+                            writer.WriteAttributeString("value", "1");
+                            var m = workspace.funcregex.Match(bit.value);
+                            if (m.Success)
+                            {
+                                if (m.Groups["Func"].Value == "ABILITYMOD")
+                                    writer.WriteAttributeString("abilmod", "true");
+                                writer.WriteAttributeString("statlink", m.Groups["Arg"].Value);
+                            }
+                            else
+                                writer.WriteAttributeString("statlink", bit.value.Trim('+'));
+                        }
                     }
                     var realval = calc(bit, workspace.Level);
                     if (SaveFileVersion > Serializer.SFVersion.v007b && realval.ToString() != bit.value)
